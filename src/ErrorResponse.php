937e721e75fbc44b3f3433ab0f323aed781924c5
William Johnston <william.johnston@drip.com>
2018-06-01 11:37:57 -0500

    Error classes

---
+++ src/ErrorResponse.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace Drip;
+
+class ErrorResponse implements ResponseInterface {
+    /** @var string */
+    private $url;
+    /** @var array */
+    private $params;
+    /** @var \Psr\Http\Message\ResponseInterface */
+    private $response;
+    /** @var array */
+    private $body;
+
+    public function __construct($url, $params, \Psr\Http\Message\ResponseInterface $response) {
+        $this->url = $url;
+        $this->params = $params;
+        $this->response = $response;
+        $this->body = json_decode($response->getBody(), true);
+    }
+
+    public function get_errors() {
+        if (!empty($this->body['errors'])) { // JSON
+            return array_map(function($rec) {
+                return new Error($rec['code'], $rec['message']);
+            }, $this->body['errors']);
+        } else {
+            return [];
+        }
+    }
+
+    /**
+     * Whether the response is successfull.
+     *
+     * @return boolean
+     */
+    public function is_success() {
+        return false;
+    }
+
+    /**
+     * The url of the request.
+     *
+     * @return string
+     */
+    public function get_url() {
+        return $this->url;
+    }
+
+    /**
+     * The parameters of the request.
+     *
+     * @return array
+     */
+    public function get_params() {
+        return $this->params;
+    }
+
+    /**
+     * The http response code
+     *
+     * @return integer
+     */
+    public function get_http_code() {
+        return $this->response->getStatusCode();
+    }
+}
---
+++ src/ResponseInterface.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Drip;
+
+interface ResponseInterface {
+    public function is_success();
+    public function get_url();
+    public function get_params();
+    public function get_http_code();
+}
